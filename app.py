d49f2eaa11a309c737156e7bc6d9dbf94f52b75f
Orges Leka <orges.leka@googlemail.com>
2020-03-19 20:21:08 +0100

    small improvement

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, Response, render_template, request
-import json,csv,logging
+import json,csv,logging, hashlib,datetime
 from wtforms import TextField, Form
 from logging.handlers import RotatingFileHandler
 
@@ -15,18 +15,50 @@ cities = []
 
 cities = ["65549"]
 
-supermaerkte = ["Aldi Werkstatt","Aldi Industriestraße Diez","Lidl Industriestraße Diez","Penny Holzheimerstraße","Penny Innenstadt","Kaufland","Globus",
-          "SB-Markt","Tegut Werkstatt","Rewe Richtung Diez","Edeka Linter","Edeka Blumenrod"]
-
-scounter = dict(zip(supermaerkte,len(supermaerkte)*[0]))
+supermaerkte = ["Aldi Werkstadt","Aldi Industriestrasse Diez","Lidl Industriestrasse Diez","Penny Holzheimerstrasse","Penny Innenstadt","Kaufland","Globus",
+          "SB-Markt","Tegut Werkstadt","Rewe Richtung Diez","Edeka Linter","Edeka Blumenrod"]
 
 
+class Supermarket:
+    def __init__(self,location,name):
+        self.location = location
+        self.name = name
+        self.list_of_warnings = []
 
+    def accept_warning(self,w,seconds=60*60*3): # eine Warnung in 3 Stunden
+        n = datetime.datetime.now()
+        old_warnings = [ ww for ww in  self.list_of_warnings if w.hashed == ww.hashed]
+        if len(old_warnings)>0:
+            m = min([ww.dtime for ww in old_warnings])
+            M = max([ww.dtime for ww in old_warnings])
+            if (n-M).total_seconds()>seconds:
+                self.list_of_warnings.append(w)
+                return True
+            else:
+                return False
+        else:
+            self.list_of_warnings.append(w)
+            return True
+        
+    
+    def delete_old_warnings(self,seconds = 60*60):
+        n = datetime.datetime.now()
+        print([str(x) for x in self.list_of_warnings])
+        self.list_of_warnings = [w for w in self.list_of_warnings if (n-w.dtime).total_seconds() > seconds]
 
+dict_of_supermarkets = dict([ ( ("65549",supermarket), Supermarket("65549",supermarket) ) for supermarket in supermaerkte])
+        
+
+class Warn:
+    def __init__(self, hashed, dtime):
+        self.hashed = hashed
+        self.dtime = dtime
+
+    def __str__(self):
+        return str(self.hashed)+"_"+str(self.dtime)
 
 class SearchForm(Form):
-    autocomp = TextField('Bitte Postleitzahl angeben', id='city_autocomplete')
-
+    autocomp = TextField('Bitte Postleitzahl angeben (65549)', id='city_autocomplete')
 
 
 @app.route('/_autocomplete', methods=['GET'])
@@ -43,23 +75,40 @@ def index():
     return render_template("search.html", form=form)
 
 
-@app.route('/increasecounter', methods=['POST'])
-def increase_counter():
-    decat = request.form['myCounter']
-    print(decat)
-    ll = list(zip(scounter.keys(),scounter.values()))
-    print(json.dumps(ll))
-    return Response(json.dumps(ll),mimetype="application/json")
+@app.route('/warn', methods=['POST'])
+def warn():
+    data = request.form
+    print(request)
+    hashed = hashlib.md5(("-".join([str(x) for x in request.headers])+request.remote_addr).encode("utf-8")).digest()
+    print(hashed)
+    dtime = datetime.datetime.now()
+    location = data["location"]
+    supermarket = data["supermarket"]
+    w = Warn(hashed,dtime)
+    S = dict_of_supermarkets[(location,supermarket)]
+    S.delete_old_warnings()
+    accepted = S.accept_warning(w)
+    dict_of_supermarkets[(location,supermarket)] = S
+    return Response(json.dumps(accepted),mimetype="application/json")
 
 
 @app.route('/receivedata', methods=['POST'])
 def receive_data():
-    decat = request.form['myData']
-    print(decat)
-    ll = list(zip(scounter.keys(),scounter.values()))
+    plz = request.form['plz']
+    ll = []
+    for key in dict_of_supermarkets.keys():
+        location, Sname = key
+        S = dict_of_supermarkets[key]
+        S.delete_old_warnings()
+        ll.append( (Sname, len(S.list_of_warnings)))         
     #print(json.dumps(scounter))
     return Response(json.dumps(ll),mimetype="application/json")
 
 if __name__ == '__main__':
     logging.basicConfig(filename="log.txt",level=logging.INFO)
-    app.run(host='192.168.178.20',port=5000)
+    STAGE = "DEV"
+    if STAGE == "DEV":
+        app.run(host='0.0.0.0',port=5000)
+    else:
+        app.run(host="192.168.178.20",port=5000)
+
