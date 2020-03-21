2b5d33f601b3b73588246fc338efdc4fe0373b2a
jade-martins <34024820+jade-martins@users.noreply.github.com>
2020-03-21 15:05:08 +0100

    Update osm_extract_shops.R

--- osm_extract_shops.R
+++ osm_extract_shops.R
@@ -14,18 +14,17 @@ header = c("osm_id","name","postcode","street","housenumber","geometry")
 write.table(header,col.names=F,row.names=F,file="test2.csv")
 for (i in 1:nrow(subset_plz)){
   print(i)
-  q <- opq(bbox = bb, timeout=30) %>%
+  q <- opq(bbox = bb) %>%
         add_osm_feature(key = 'shop', value = 'supermarket') %>%
         add_osm_feature(key = 'addr:postcode', value = as.character(subset_plz$zipcode[i]))
   dat = osmdata_sf(q)
   dat2 = dat$osm_points
-  dat2 = dat2[! is.na(dat2$addr.street),]
-  dat2 = dat2[! is.na(dat2$addr.housenumber),]
   dat3 = dat$polygones
-  dat3[! is.na(dat3$addr.street),]
-  dat3[! is.na(dat3$addr.housenumber),]
   dat_filt = cbind(dat2$addr.postcode,dat2$name,dat2$addr.street,dat2$addr.housenumber, dat2$geometry,dat2$osm_id)
-  write.csv2(dat_filt,append=T,col.names=F,row.names=F,file="test2.csv",na="NA")
+  write.table(dat_filt,append=T,col.names=F,row.names=F,file="test2.csv",sep=";")
   dat_filt = cbind(dat3$addr.postcode,dat3$name,dat3$addr.street,dat3$addr.housenumber, dat3$geometry,dat3$osm_id)
-  write.csv2(dat_filt,append=T,col.names=F,row.names=F,file="test2.csv",na="NA")
+  write.table(dat_filt,append=T,col.names=F,row.names=F,file="test2.csv",sep=";")
 }
+
+#clean data in cmdline, osm data has many duplicates and uncomplete entries
+#grep '^[0-9]' test2.csv  | cut -f 1,2,3,4 -d ";" | sort | uniq
